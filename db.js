b77d036ab867f2b9bcd4292141c8bc0f8dd7297c
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-05 01:12:55 +0600

    create todos

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
+  user: "postgres",
+  password: "15204008",
   host: "localhost",
-  user: "database-user",
   port: 5432,
-  database: "bookDB",
+  database: "perntodo",
 });
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db");
 const app = express();
 app.use(express.json());
 app.use(cors());
 const port = 5000;
 
+app.post("/todos", async (req, res) => {
+  try {
+    console.log(req.body);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+//
+
 app.listen(port, () => {
   console.log(`Application listening on port ${port}`);
 });
+
+pool;
