3f5ad1206174960dab9cdd2277a5c2f7f03932b8
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-05 02:02:25 +0600

    get a todo by id

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(cors());
 const port = 5000;
 
-
+// get all todos
 app.get("/todos", async (req, res) => {
   try {
     const allTodos = await pool.query("SELECT * FROM todo");
@@ -16,6 +16,20 @@ app.get("/todos", async (req, res) => {
   }
 });
 
+// get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// Create a new todo
 app.post("/todos", async (req, res) => {
   try {
     const { description } = req.body;
